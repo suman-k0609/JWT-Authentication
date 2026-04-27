e4d214ec46abf1901313dfb9b1ce0f01b67863fd
Suman Kumari <sumankumari200603@gmail.com>
2026-04-27 23:24:52 +0530

    initial commit

---
+++ src/db/db.js
@@ -0,0 +1,11 @@
+const mongoose=require('mongoose');
+async function connectDB(){
+    try{
+       await mongoose.connect(process.env.MONGO_URI)
+       console.log("databse connected successfully"); 
+    }
+    catch(err){
+        console.error("Database connection error:",err);
+    }
+}
+module.exports=connectDB
